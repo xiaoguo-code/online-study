65d6b03727b38f8c6a23e1f596a90e228b3a1ac0
guoyr <410718730@qq.com>
2021-02-28 23:38:18 +0800

    项目基本架构

---
+++ src/main/java/com/gyr/onlinestudy/common/aspect/UserProxy.java
@@ -0,0 +1,18 @@
+package com.gyr.onlinestudy.common.aspect;
+
+/**
+ * @Author: guoyongrun
+ * @Date: 2020/12/6 23:55
+ * @Description：aop代理增强类
+ * @Version: 1.0
+ */
+
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect//生成代理对象
+public class UserProxy {
+
+
+}
---
+++ src/test/java/com/gyr/onlinestudy/OnlineStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyr.onlinestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
